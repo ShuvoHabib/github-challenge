7300cbc936f02f3bde0a98a403acab8b83c5531a
shuvo habib <shuvohabib@gmail.com>
2017-05-25 00:14:22 +0600

    Add router in project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React, {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 import Popular from './Popular';
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Nav = () => (
+  <div>
+    <h2>Nav</h2>
+        <ul>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/popular">Popular</Link>
+        </ul>
+  </div>
+);
 
 class App extends Component {
     render() {
         return (
+        <Router>
             <div className='container'>
-                <Popular />
+                <Nav />
+                <Route exact path="/" component={Home}/>
+                <Route path="/popular" component={Popular}/>
+                <Route path="/about" component={About}/>
             </div>
+        </Router>
+
         )
     }
 }
